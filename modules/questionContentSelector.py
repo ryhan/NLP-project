f8070a42766d843245e4884ab91ceb73d857cebc
Ryhan <rthprog@Gmail.com>
2013-02-14 02:07:36 -0500

    Import sample modules from /modules

---
+++ modules/questionContentSelector.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+# questionContentSelector.py
+# Authored by Ryhan Hassan | rhassan@andrew.cmu.edu
+# Identifies declarative sentences in source text
+# that are candidates for question generation.
+
+import nltk
+
+# GIVEN source_text string
+# RETURNS list of candidate strings
+def process(source_text):
+  return [ ]
