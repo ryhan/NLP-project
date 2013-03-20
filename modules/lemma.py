0e301b283cff9bac5d6eabc0374914fefc20c817
Daniel Sedra <danielsedra@aol.com>
2013-03-20 14:16:39 -0400

    Fixed lemma.py POS

--- modules/lemma.py
+++ modules/lemma.py
@@ -1,20 +1,33 @@
 # takes in a word list and returns a list of lists
 # each nested list is a list of lemmas for each word
 
-# TODO
-# can be refined by POS tagging
-
+import nltk
 from nltk.corpus import wordnet as wn
 
 def lem(list):
+
+  pos = nltk.pos_tag(list)
+
   lemmaList = []
 
-  for word in list:
-    temp = []
-    for ss in wn.synsets(word):
-      temp += [lemma.name for lemma in ss.lemmas]
-    lemmaList.append(temp)
-  
-  print lemmaList
+  for word,tag in pos:
+    temp = set({word})
+    
+    if tag[0] == 'N':
+      for ss in wn.synsets(word, pos=wn.NOUN):
+        temp.update(set(lemma.name for lemma in ss.lemmas))
+      lemmaList.append(temp)
+    elif tag[0] == 'V':
+      for ss in wn.synsets(word, pos=wn.VERB):
+        temp.update(set(lemma.name for lemma in ss.lemmas))
+      lemmaList.append(temp)
+    else:
+      lemmaList.append({word})
 
-lem(['hello','world'])
+  
+  return lemmaList
+
+
+lem(['I','destroyed','the','facility'])
+
+
